4a6b3e95901cb38af6cb261b211eb3191324d831
David Pullinger <davpullinger@gmail.com>
2025-05-18 16:40:12 +0200

    feat: add keymapping config, and remove homebrew source

--- initial_setup.zsh
+++ initial_setup.zsh
@@ -4,6 +4,7 @@
 echo > ~/.zshenv
 echo 'export XDG_CONFIG_HOME="$HOME/.config"' >> ~/.zshenv
 echo 'export ZDOTDIR="$XDG_CONFIG_HOME/zsh"' >> ~/.zshenv && mkdir -p ~/.config/zsh
+source ~/.zshenv
 
 # 2. Homebrew
 if ls /opt/homebrew > /dev/null 2>&1; then # I prefer to do this to check for brew
@@ -12,13 +13,10 @@ else
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshenv
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# 3. Source env from 1 and 2
-source ~/.zshenv
-
-# 4. Misc. cli tools & apps
-## 4.1 formulae
+# 3. Misc. cli tools & apps
+## 3.1 formulae
 brew install "git"
 brew install "fzf"
 brew install "fd"
@@ -28,7 +26,7 @@ brew install "tmux"
 brew install "jq"
 brew install "koekeishiya/formulae/skhd"
 brew install "koekeishiya/formulae/yabai"
-## 4.2 casks
+## 3.2 casks
 brew install --cask "ghostty"
 brew install --cask "zen"
 brew install --cask "spotify"
@@ -36,3 +34,13 @@ brew install --cask "1password"
 brew install --cask "vlc"
 brew install --cask "raycast"
 brew install --cask "docker"
+
+# 4. Some important config for some of the above
+echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
+
+# 5. Some macos config
+mkdir -p ~/Library/LaunchAgents
+cp plist/* ~/Library/LaunchAgents
+
+launchctl unload ~/Library/LaunchAgents/
+launchctl load ~/Library/LaunchAgents/
