4406a87f003275fd828b5706cee68c252681527a
David Pullinger <davpullinger@gmail.com>
2025-05-21 07:47:42 +0200

    feat: use brewfiles and brew bundle

--- initial_setup.zsh
+++ initial_setup.zsh
@@ -6,7 +6,7 @@ echo 'export XDG_CONFIG_HOME="$HOME/.config"' >> ~/.zshenv
 echo 'export ZDOTDIR="$XDG_CONFIG_HOME/zsh"' >> ~/.zshenv && mkdir -p ~/.config/zsh
 source ~/.zshenv
 
-# 2. Homebrew
+# 2. Homebrew install
 if ls /opt/homebrew > /dev/null 2>&1; then # I prefer to do this to check for brew
     echo "Homebrew already installed."
 else
@@ -15,31 +15,11 @@ else
 fi
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# 3. Misc. cli tools & apps
-## 3.1 formulae
-brew install "git"
-brew install "fzf"
-brew install "fd"
-brew install "ripgrep"
-brew install "neovim"
-brew install "tmux"
-brew install "jq"
-brew install "koekeishiya/formulae/skhd"
-brew install "koekeishiya/formulae/yabai"
-### JS
-brew install "node"
-brew install "yarn"
-### PHP
-brew install "composer"
-## 3.2 casks
-brew install --cask "ghostty"
-# brew install --cask "zen" (when DRM)
-brew install --cask "spotify"
-brew install --cask "1password"
-brew install --cask "vlc"
-brew install --cask "raycast"
-brew install --cask "docker"
-brew install --cask "arc"
+# 3. Formulae, casks and taps
+HOMEBREW_BUNDLE_FILE_GLOBAL=$(pwd)/brew/formulae.brewfile
+brew bundle check --global || brew bundle install --global
+HOMEBREW_BUNDLE_FILE_GLOBAL=$(pwd)/brew/casks.brewfile
+brew bundle check --global || brew bundle install --global
 
 # 4. Some important config for some of the above
 echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
