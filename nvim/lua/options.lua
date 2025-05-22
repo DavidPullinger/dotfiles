84db0864d431558effd0b7ccc01ab300fba87148
David Pullinger <davpullinger@gmail.com>
2025-05-22 19:22:01 +0200

    feat: adding some sane options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,5 +1,14 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.opt.relativenumber = true
-vim.opt.tabstop = 4
+vim.o.relativenumber = true
+vim.o.number = true
+
+vim.o.tabstop = 4
+vim.o.breakindent = true
+
+vim.o.hlsearch = false
+
+vim.o.clipboard = "unnamedplus"
+
+vim.o.undofile = true
