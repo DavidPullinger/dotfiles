3322cf0833b41b2626a2b284e61ff62ab30be59b
David Pullinger <davpullinger@gmail.com>
2025-05-22 19:21:06 +0200

    feat: add some more search funcs to telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,10 +1,23 @@
+local function find_folders()
+  require('telescope.builtin').find_files({
+    find_command = { 'fd', '--type', 'd', '--hidden', '--follow', '--exclude', '.git' },
+    prompt_title = 'Find Folders',
+  })
+end
+
 return {
     'nvim-telescope/telescope.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     init = function() 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Find files' })
+        -- actual searching --
+        vim.keymap.set('n', '<leader>sf', builtin.fd, { desc = 'Find files' })
+        vim.keymap.set('n', '<leader>sF', find_folders, { desc = 'Find folders' })
         vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Grep' })
         vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Find buffer' })
+        vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = 'Find in current buffer' })
+        vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = 'Find old files' })
+        -- QOL --
+        vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = 'Find in current buffer' })
     end
 }
