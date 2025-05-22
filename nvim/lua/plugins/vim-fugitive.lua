acf5c01081d3576ea6f42a2a2c9ea4d38ee66df7
David Pullinger <davpullinger@gmail.com>
2025-05-22 19:21:35 +0200

    feat: add git push to fugitive

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -3,6 +3,7 @@ return {
     init = function()
         vim.keymap.set('n', '<leader>gs', function() vim.cmd('Git') end, { desc = 'Git status' })
         vim.keymap.set('n', '<leader>gc', function() vim.cmd('Git commit') end, { desc = 'Git commit' })
+        vim.keymap.set('n', '<leader>gp', function() vim.cmd('Git push') end, { desc = 'Git push' })
         vim.keymap.set('n', '<leader>gb', function() vim.cmd('Git blame') end, { desc = 'Git blame' })
     end
 }
