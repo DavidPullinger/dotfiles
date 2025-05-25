f01cb98d5d9538223f52e4dd9932b2257eb90ede
David Pullinger <davpullinger@gmail.com>
2025-05-25 10:10:06 +0200

    feat: add fzf extension for telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,17 +1,23 @@
 local function find_folders()
-  require('telescope.builtin').find_files({
-    find_command = { 'fd', '--type', 'd', '--hidden', '--follow', '--exclude', '.git' },
-    prompt_title = 'Find Folders',
-  })
+    require('telescope.builtin').find_files({
+        find_command = { 'fd', '--type', 'd', '--hidden', '--follow', '--exclude', '.git' },
+        prompt_title = 'Find Folders',
+    })
 end
 
 return {
     'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+        },
+    },
     init = function() 
         local builtin = require('telescope.builtin')
         -- actual searching --
-        vim.keymap.set('n', '<leader>sf', builtin.fd, { desc = 'Find files' })
+        vim.keymap.set('n', '<leader>sf', function() builtin.fd({ hidden = true }) end, { desc = 'Find files' })
         vim.keymap.set('n', '<leader>sF', find_folders, { desc = 'Find folders' })
         vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Grep' })
         vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Find buffer' })
@@ -19,5 +25,8 @@ return {
         vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = 'Find old files' })
         -- QOL --
         vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = 'Find in current buffer' })
-    end
+    end,
+    config = function()
+        require("telescope").load_extension("fzf")
+    end,
 }
