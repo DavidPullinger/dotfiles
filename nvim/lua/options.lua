d0288e58ba9853a43219a196286d66412b7b298d
David Pullinger <davpullinger@gmail.com>
2025-05-27 04:39:08 +0200

    chore: add some more options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -5,7 +5,11 @@ vim.o.relativenumber = true
 vim.o.number = true
 
 vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 vim.o.breakindent = true
+
+-- hmmm, probably controversial
+vim.opt.iskeyword:append("-")
 
 vim.o.hlsearch = false
 
