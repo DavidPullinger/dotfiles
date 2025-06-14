9177d8a470f29cf2c430c58057c96aaa7ff51fc0
David Pullinger <davpullinger@gmail.com>
2025-06-14 07:26:49 +0200

    feat: add useful mappings for qf list

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+-- for navigating quick fix list
+vim.keymap.set("n", "<C-j>", ":cnext<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", ":cprev<CR>", { silent = true })
