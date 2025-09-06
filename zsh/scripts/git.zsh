35604b93cdd9eee583152ccc17a62dadac658771
David Pullinger <davpullinger@gmail.com>
2025-09-06 11:18:15 +0200

    chore: clean up tmux & add git/tmux scripts

--- zsh/scripts/git.zsh
+++ zsh/scripts/git.zsh
@@ -1,9 +1,26 @@
 # for git pull
 gp() {
-	gsh && git pull origin $(git branch --show-current) --rebase && gsp
+	has_changes=$(git status --porcelain --untracked-files=no)
+	
+	if [[ -n $has_changes ]]; then
+		gsh
+	fi
+
+	git pull origin $(git branch --show-current) --rebase
+
+	if [[ -n $has_changes ]]; then
+		gsp
+	fi
 }
 
-# for git pull without stash
-gpp() {
-	git pull origin $(git branch --show-current) --rebase
+git_new() {
+	read "branch?Enter new git branch: "
+
+	if [ -n "$branch" ]; then
+		(git switch master && \
+		git pull origin master && \
+		git checkout -b "$branch") || read # if anything goes wrong, just allow us to read the error
+	else
+		echo "No branch entered. Press Enter to exit..."; read
+	fi
 }
--- zsh/scripts/tmux.zsh
+++ zsh/scripts/tmux.zsh
@@ -1,3 +1,5 @@
+# lazy me... creates a bunch of tmux sessions in common directories
+# each with one nvim window and one zsh window to boot
 function tmx() {
 	typeset -A sessions
 
@@ -7,16 +9,17 @@ function tmx() {
 	sessions[rates-manager]=~/work/stack/services/rates-manager/
 	sessions[tool]=~/work/stack/tool/
 	sessions[scratch_pad]=~/work/scratch_pad/
+	sessions[config]=~/.config/
 
 	# create sessions, and on each create a vim and zsh window
 	for session in ${(k)sessions}; do
 		if ! tmux has-session -t $session 2>/dev/null; then
-		    tmux new-session -d -s $session -c "$sessions[$session]"
+			tmux new-session -d -s $session -c "$sessions[$session]"
 			# nvim window
 			tmux rename-window -t $session:0 nvim
-            tmux send-keys -t $session:nvim "nvim ." C-m
+			tmux send-keys -t $session:nvim "nvim ." C-m
 			# zsh window
-		    tmux new-window -t $session -n zsh -c "$sessions[$session]" "zsh"
+			tmux new-window -t $session -n zsh -c "$sessions[$session]" "zsh"
 		fi
 	done
 
@@ -24,26 +27,51 @@ function tmx() {
 	tmux attach-session -t global
 }
 
-function t() {
-	local directories=(~/work ~/personal)
-	local selected_dir=$(fd ".*" --type d $directories | sed "s|$HOME/||g" | fzf)
+# fuzzy find across subdirs in ~/work and ~/personal
+# create new session with the same name as selected directory
+function tmux_new() {
+	local directories=(~/work ~/personal) # make more dynamic? Maybe ~/ with some smart excludes
+	local selected_dir=$(fd ".*" --type d $directories | sed "s|$HOME/|~/|g" | fzf)
+	selected_dir=${selected_dir/\~/$HOME}
+
 	if [ -z "$selected_dir" ]; then
-		return 1
+		return 0
 	fi
 
 	local session_name=$(basename "$selected_dir")
-	tmux new-session -s "$session_name" -c "$HOME/$selected_dir"
+	
+	# check for exact match (-t=)
+	if tmux has-session -t="$session_name" 2>/dev/null; then
+		tmux switch-client -t "$session_name"
+		return 0
+	fi
+
+	# create the session detached so that I can call this from inside tmux
+	tmux new-session -d -s "$session_name" -c "$selected_dir"
+
+	if [ -z "$TMUX" ]; then
+		tmux attach-session -t $session_name
+	else
+		tmux switch-client -t $session_name
+	fi
 }
 
-function ts() {
-	local selected_dir=$(tmux list-sessions -F \#S | fzf)
-	if [ -z "$selected_dir" ]; then
-		return 1
+# fuzzy find across current sessions
+# if we're attached already, switch to selection
+# otherwise, attach to selection
+function tmux_switch() {
+	sessions=$(tmux list-sessions -F '#S')
+	current=$(tmux display-message -p '#S')
+	target=$(echo "$sessions" | rg --invert-match "${current}" | fzf --reverse)
+
+	# if we didn't select anything, exit
+	if [[ -z "$target" ]]; then
+		return 0
 	fi
 
 	if [ -z "$TMUX" ]; then
-		tmux attach-session -t $selected_dir
+		tmux attach-session -t $target
 	else
-		tmux switch-client -t $selected_dir
+		tmux switch-client -t $target
 	fi
 }
